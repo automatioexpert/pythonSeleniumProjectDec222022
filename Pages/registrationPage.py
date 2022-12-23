d0015a981dda6e40d19430d80d164ff1a4bdd443
Valmiki Vishwakarma <you@example.com>
2022-12-23 09:49:39 +0530

    adding the updated registrationPage.py file.

--- Pages/registrationPage.py
+++ Pages/registrationPage.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
-
 class registration:
 
     def __init__(self, driverObj):
@@ -11,5 +9,7 @@ class registration:
 
     def enterDataByName(self, locator, data):
         driver.find_element(By.NAME, locator).send_keys(data)
+
+
 
 
